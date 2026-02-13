d66aea4a5242d20c308f8c7cd0a0e86c177fce93
Bharat <bdhande45@gmail.com>
2026-02-14 01:40:14 +0530

    update chatbot url

--- chatbot-script.js
+++ chatbot-script.js
@@ -100,7 +100,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     try {
 
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("http://13.127.173.23:8000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
