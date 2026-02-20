4aaf42fbd1fc66ee234f03c873801693f70cd027
Bharat <bdhande45@gmail.com>
2026-02-20 21:47:53 +0530

    url changed

--- chatbot-script.js
+++ chatbot-script.js
@@ -100,8 +100,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     try {
 
-      //const response = await fetch("https://register-approximately-dubai-kits.trycloudflare.com/chat", {
-        const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("https://register-approximately-dubai-kits.trycloudflare.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
