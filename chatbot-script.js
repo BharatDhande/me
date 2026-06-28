cb1a50591e22c0fc8979e290a34420f9244c5660
Bharat <bdhande45@gmail.com>
2026-06-28 23:48:14 +0530

    change bot url

--- chatbot-script.js
+++ chatbot-script.js
@@ -100,7 +100,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     try {
 
-      const response = await fetch("https://lets-par-theology-languages.trycloudflare.com/chat", {
+      const response = await fetch("https://me-z2k6.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
